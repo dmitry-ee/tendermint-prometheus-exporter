bbd205d69ae41527b243a966ef58ea33d52c0316
Dmitry Evdokimov <devdokimov@alfabank.ru>
2020-03-24 16:12:44 +0300

    fix file checks

--- provision/provision.sh
+++ provision/provision.sh
@@ -7,12 +7,16 @@ mkdir -p $MINTER_DATA_DIR/config
 chown -R 999:1000 $MINTER_DATA_DIR
 
 if [ -f "$MINTER_DATA_DIR/config/config.toml" ]; then
+  echo "config.toml exists..."
+else
   # generate config
   echo "$MINTER_DATA_DIR/config/config.toml does not exists!"
   docker run --name minter-config-gen --rm -v $MINTER_DATA_DIR:/minter dmi7ry/minter-node:$MINTER_NODE_VERSION
 fi
 
 if [ -f "$MINTER_DATA_DIR/config/genesis.json" ]; then
+  echo "genesis.json exists..."
+else
   # download genesis file
   echo "$MINTER_DATA_DIR/config/genesis.json does not exists, going to download!"
   docker run --name minter-genesis-fetch --rm -v $MINTER_DATA_DIR:/minter dmi7ry/minter-node:$MINTER_NODE_VERSION fetch-genesis
