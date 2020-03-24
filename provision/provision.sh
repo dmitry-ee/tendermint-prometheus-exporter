2f79c11f1bae7609af1018b1f110e082b366b9ff
Dmitry Evdokimov <devdokimov@alfabank.ru>
2020-03-24 16:08:27 +0300

    fix dirs & vars

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,19 +3,19 @@
 MINTER_DATA_DIR="/minter"
 MINTER_NODE_VERSION="latest"
 
-mkdir -p $MINTER_DIR/config
-chown -R 999:1000 $MINTER_DIR
+mkdir -p $MINTER_DATA_DIR/config
+chown -R 999:1000 $MINTER_DATA_DIR
 
-if [ -f "$MINTER_DIR/config/config.toml" ]; then
+if [ -f "$MINTER_DATA_DIR/config/config.toml" ]; then
   # generate config
-  echo "$MINTER_DIR/config/config.toml does not exists!"
-  docker run --name minter-config-gen --rm -v $MINTER_DIR:/minter dmi7ry/minter-node:$MINTER_VERSION
+  echo "$MINTER_DATA_DIR/config/config.toml does not exists!"
+  docker run --name minter-config-gen --rm -v $MINTER_DATA_DIR:/minter dmi7ry/minter-node:$MINTER_NODE_VERSION
 fi
 
-if [ -f "$MINTER_DIR/config/genesis.json" ]; then
+if [ -f "$MINTER_DATA_DIR/config/genesis.json" ]; then
   # download genesis file
-  echo "$MINTER_DIR/config/genesis.json does not exists, going to download!"
-  docker run --name minter-genesis-fetch --rm -v $MINTER_DIR:/minter dmi7ry/minter-node:$MINTER_VERSION fetch-genesis
+  echo "$MINTER_DATA_DIR/config/genesis.json does not exists, going to download!"
+  docker run --name minter-genesis-fetch --rm -v $MINTER_DATA_DIR:/minter dmi7ry/minter-node:$MINTER_NODE_VERSION fetch-genesis
 fi
 
 docker-compose up -d
