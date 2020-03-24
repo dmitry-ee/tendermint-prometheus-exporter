53dc3615953c7ae1afc12f45d2dc0c5e96679163
Dmitry Evdokimov <devdokimov@alfabank.ru>
2020-03-24 16:04:46 +0300

    add env vars

--- provision/provision.sh
+++ provision/provision.sh
@@ -8,11 +8,13 @@ chown -R 999:1000 $MINTER_DIR
 
 if [ -f "$MINTER_DIR/config/config.toml" ]; then
   # generate config
+  echo "$MINTER_DIR/config/config.toml does not exists!"
   docker run --name minter-config-gen --rm -v $MINTER_DIR:/minter dmi7ry/minter-node:$MINTER_VERSION
 fi
 
 if [ -f "$MINTER_DIR/config/genesis.json" ]; then
   # download genesis file
+  echo "$MINTER_DIR/config/genesis.json does not exists, going to download!"
   docker run --name minter-genesis-fetch --rm -v $MINTER_DIR:/minter dmi7ry/minter-node:$MINTER_VERSION fetch-genesis
 fi
 
