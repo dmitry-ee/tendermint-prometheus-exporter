5fb28e7cbb6601cdd3b8fab105b485e6c21d824e
Dmitry Evdokimov <devdokimov@alfabank.ru>
2020-03-30 19:07:12 +0300

    fix fetch genesis

--- provision/provision-with-node.sh
+++ provision/provision-with-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MINTER_DATA_DIR="/minter"
-export MINTER_NODE_VERSION="latest"
+export MINTER_NODE_VERSION="1.1.5"
 
 mkdir -p $MINTER_DATA_DIR/config
 chown -R 999:1000 $MINTER_DATA_DIR
@@ -13,14 +13,6 @@ else
   echo "$MINTER_DATA_DIR/config/config.toml does not exists!"
   docker run --name minter-config-gen --rm -v $MINTER_DATA_DIR:/minter dmi7ry/minter-node:$MINTER_NODE_VERSION show_validator
   sed -i 's/prometheus = false/prometheus = true/' $MINTER_DATA_DIR/config/config.toml
-fi
-
-if [ -f "$MINTER_DATA_DIR/config/genesis.json" ]; then
-  echo "genesis.json exists..."
-else
-  # download genesis file
-  echo "$MINTER_DATA_DIR/config/genesis.json does not exists, going to download!"
-  docker run --name minter-genesis-fetch --rm -v $MINTER_DATA_DIR:/minter dmi7ry/minter-node:$MINTER_NODE_VERSION fetch-genesis
 fi
 
 docker-compose -f docker-compose-with-node.yml up -d --remove-orphans
