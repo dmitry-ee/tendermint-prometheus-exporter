00b11f131d899c32048b5b3ac6dab6fccb9884ab
Dmitry Evdokimov <devdokimov@alfabank.ru>
2020-03-31 21:46:41 +0300

    remove sync scraper

--- src/lib/metric-server.js
+++ src/lib/metric-server.js
@@ -29,7 +29,6 @@ class MetricServer {
       _targetOpts: [],
       _timeout: 1000,
       _httpMetricsPerfix: 'minter',
-      _scrapeAsync: true,
       _metricsRetention: 3600000,
     }, options)
 
@@ -64,10 +63,7 @@ class MetricServer {
         }
         return next()
       })
-      if (self._scrapeAsync)
-        seq.append( self.asyncScraperBuilder(self._targets, self._metrics, self._timeout) )
-      else
-        self.syncScraperBuilder(self._targets, self._metrics).forEach(scraper => seq.append(scraper))
+      seq.append(self.asyncScraperBuilder(self._targets, self._metrics, self._timeout))
       seq.append(self._api.bind(null, req, res))
       seq.run()
     })
@@ -156,78 +152,6 @@ class MetricServer {
         next()
       })
     }
-  }
-
-  syncScraperBuilder(targets, metrics) {
-
-    logger.info('#syncScraperBuilder - building scrapers...')
-    let scrapers = []
-
-    targets.forEach(target => {
-      if (target.status) {
-        scrapers.push(
-          (req, res, next) => this.scrape(`${target.url}/status`, (err, data) => {
-            if (err) {
-              next(err)
-              return
-            }
-            logger.info(`scrape:sync -> parsing response from /status`)
-            let moniker = data.result.tm_status.node_info.moniker
-            metrics.setLabels({ moniker: moniker, target: target.url })
-            metrics.set('status', {}, data)
-            next()
-          })
-        )
-      }
-      if (target.netInfo || target['net-info']) {
-        scrapers.push(
-          (req, res, next) => this.scrape(`${target.url}/net_info`, (err, data) => {
-            if (err) {
-              next(err)
-              return
-            }
-            logger.info(`scrape:sync -> parsing response from /net_info`)
-            metrics.setLabels({ target: target.url })
-            metrics.set('net_info', { target: target.url }, data)
-            next()
-          })
-        )
-      }
-      if (target.candidates) {
-        scrapers.push(
-          (req, res, next) => this.scrape(`${target.url}/candidates`, (err, data) => {
-            if (err) {
-              next(err)
-              return
-            }
-            logger.info(`scrape:sync -> parsing response from /candidates`)
-            metrics.setLabels({ target: target.url })
-            metrics.set('candidates', { target: target.url }, data)
-            next()
-          })
-        )
-      }
-    })
-
-    logger.info(`#syncScraperBuilder - success!`)
-    logger.info(scrapers)
-    return scrapers
-  }
-
-  scrape(scrapeUrl, callback) {
-    logger.info(`scrape:sync -> performing request to ${scrapeUrl}`)
-    request({ url: scrapeUrl, time: true }, (err, resp) => {
-      logger.info(`scrape:sync -> got data from ${scrapeUrl}`)
-      HttpMetricsCollector.collect(err || resp)
-      try {
-        let body = JSON.parse(resp.body)
-        callback(null, body)
-      } catch (e) {
-        logger.error(resp.body)
-        logger.error(e)
-        callback(e, resp.body)
-      }
-    })
   }
 }
 
